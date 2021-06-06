cce06890d98da537f3153e7aef94a36ba7e12251
Corbin Garner <Corbingar@gmail.com>
2021-06-06 00:50:20 -0700

    lighting up the background and fixing the dark text

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
                 <h2>Contact</h2>
             </div>
             <div>
-            <h5 className="card-body contactSubheading">Feel free to send me an email at coletta.mike@gmail.com.</h5>
+            <h5 className="card-body contactSubheading">Feel free to send me an email at CorbinGar@gmail.com.</h5>
             </div>
             <div className="row iconRow">
                 <div className="card col-6 iconCard">
