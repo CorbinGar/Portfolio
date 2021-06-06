7f1134c3daa104b852929cad302f5dc847016ce3
Corbin Garner <Corbingar@gmail.com>
2021-06-06 00:53:54 -0700

    commit

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -7,28 +7,14 @@ function Navbar() {
 <nav className="navbar navbar-dark sticky-top navbar-expand-md">
 <div className="container-fluid">
 
-        <h1 className="navbar-name">Corbin Garner</h1>
+           <button><h1 className="navbar-name">Corbin Garner</h1></button>
     
-    <button className="navbar-toggler dropdownButton" type="button" data-toggle="collapse" data-target="#portfolio-navbar"
-        aria-controls="portfolio-navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon dropdownIcon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="portfolio-navbar">
-        <ul className="navbar-nav ml-auto">
-            <li className="dropdown-item">
+            <div>
                 <Link to="/about" className="navlink">About</Link>
-            </li>
-            <li className="dropdown-item">
                 <Link to="/portfolio" className="navlink">Portfolio</Link>
-            </li>
-            <li className="dropdown-item">
                 <Link to="/experience" className="navlink">Experience</Link>
-            </li>
-            <li className="dropdown-item">
                 <Link to="/contact" className="navlink">Contact</Link>
-            </li>
-        </ul>
-    </div>
+            </div>
 </div>
 </nav>
     )
