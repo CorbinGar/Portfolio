ec22154a18172ff7baa79176aacdb064cc72e709
Corbin Garner <Corbingar@gmail.com>
2021-06-05 23:35:55 -0700

    changes to cards

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,9 +1,9 @@
-import theRelocator from "./images/TheRelocator.JPG";
-import dankReviews from "./images/DankReviews.JPG";
-import codingQuiz from "./images/CodingQuiz.JPG";
-import weatherDashboard from "./images/WeatherDashboard.JPG";
-import employeeDirectory from "./images/EmployeeDirectory.JPG";
-import budgetTracker from "./images/OnlineOfflineBudgetTracker.JPG";
+import gramagram from "./images/Gramagram.png";
+import Schedule from "./imagesSchedule.png";
+import FitnessTracker from "./images/FitnessTracker.png";
+import weatherDashboard from "./images/weather-dashboard.png";
+import employeeDirectory from "./images/EmployeeTracker.png";
+import passwordgenarator from "./images/passwordgenarator.png";
 
 
 const portfolioProjects = [
@@ -15,41 +15,40 @@ const portfolioProjects = [
         The application pulls between 10-20 users using the Random User Generator API. 
         You can then filter and sort users by hire date, first name, last name, email and phone number.`,
         tech: "React.js, Random User Generator API, Javascript",
-        link: "https://mikecoletta.github.io/Employee-Directory/",
-        github: "https://github.com/MikeColetta/Employee-Directory"
+        link: "https://corbingar.github.io/HW19-employee-directory/",
+        github: "https://github.com/CorbinGar/HW19-employee-directory"
     },
     {
-        title: "Dank Reviews",
-        img: dankReviews,
-        heading: "Review Memes from Reddit!",
-        description: `Dank Reviews lets users create an account and review silly
-        memes from Reddit.
-        It uses the reddit.images NPM package to pull a random meme. Users can see other users
-        reviews and all of the reviews
-        are stored in a MySQL database.`,
-        tech: "Reddit.images API, Sequelize, Bcrypt, DOTENV, MySQL2, Express, Handlebars, Session, MySQL",
-        link: "https://bootcamp-project2-uw.herokuapp.com/",
-        github: "https://github.com/kharam/Dank-Reviews"
+        title: "Fitness Tracker",
+        img: FitnessTracker,
+        heading: "Helps keep you fit!",
+        description: `If you want to keep track out how far youve ran or swam or both and all in an
+        easy to use and simplistic package fitness tracker is for you just like this explanation its sweet and simple`,
+        tech: "API, MongoDb, Express, Reactjs, nodejs",
+        link: "https://salty-coast-03002.herokuapp.com/",
+        github: "https://github.com/CorbinGar/HW17-WorkoutTracker"
     },
     {
-        title: "Online/Offline Budget Tracker",
-        img: budgetTracker,
-        heading: "Track your expenses online and offline!",
-        description: `This online/offline budget tracker uses IndexedDB to store user
-         input data offline, so when the user is back online, the IndexedDB data is added to the MongoDB.`,
+        title: "gramagram",
+        img: gramagram,
+        heading: "Grandmas social media",
+        description: `Are you tired of old grandmas taking up your social media feeds and 
+        accidentally liking horrible news, well we have a solution gramagram, the socail media for the elderly
+        with large easily readble text and big hard to miss buttons your grandmother will love it`,
         tech: "IndexedDB, MongoDB, Javascript, Express",
-        link: "https://thawing-mesa-54023.herokuapp.com/",
-        github: "https://github.com/MikeColetta/Online-Offline-Budget-Trackers"
+        link: "https://boiling-savannah-28129.herokuapp.com",
+        github: "https://github.com/rsowald/GrammaGram"
     },
     {
-        title: "The Relocator",
-        img: theRelocator,
-        heading: "Find your forever home!",
-        description: `The Relocator is an app for users that want to compare two cities. The app compares the location, cost of living, and weather data of two cities. The
-        Relocator also stores previous user searches for later use.`,
-        tech: "W3 CSS Framework, JQuery, Open Weather API, Numbeo API, OpenStreetMap API, MapBox, Day.js",
-        link: "https://mikecoletta.github.io/Group-Project-One/",
-        github: "https://github.com/MikeColetta/Group-Project-One"
+        title: "Daily Scheduler",
+        img: Schedule,
+        heading: "Helping keep you organized",
+        description: `the daily scheduler has an easy and intuitive interface for planning
+        out you day, even highlighting the current hour in green and past hours in red making it
+        much easier to at a glance , know what your whole day looks like`,
+        tech: "HTML5, Bootstrap, Javascript",
+        link: "https://corbingar.github.io/5-DailyDashboard.github.io/",
+        github: "https://github.com/CorbinGar/5-DailyDashboard/"
     },
     {
         title: "Weather Dashboard",
@@ -60,19 +59,20 @@ const portfolioProjects = [
         color coded based on the severity level. The user's search history is
         saved in local storage.`,
         tech: "HTML5, Bootstrap, Javascript, Open Weather API",
-        link: "https://mikecoletta.github.io/Weather-Dashboard/",
-        github: "https://github.com/MikeColetta/Weather-Dashboard"
+        link: "https://corbingar.github.io/6-WeatherDashboard.github.io/",
+        github: "https://github.com/CorbinGar/6-WeatherDashboard.github.io"
     },
     {
-        title: "Coding Quiz",
-        img: codingQuiz,
-        heading: "Test your coding knowledge!",
-        description: `When the player starts the game they have 75 seconds to
-        complete the quiz. Every wrong answer takes time off the clock. The final score is the time
-        left on the clock. High scores are saved to local storage.`,
+        title: "Password generator",
+        img: passwordgenarator,
+        heading: "Security is always importand",
+        description: `you give this program the charcters and lenth of your
+        desired password and it will give you a randomly generated password that is
+        gauranteed to include  at least one of types of charcters you mention, for those
+        pesky special charcter required passwords`,
         tech: "HTML5, Bootstrap, Javascript",
-        link: "https://mikecoletta.github.io/Code-Quiz/",
-        github: "https://github.com/MikeColetta/Code-Quiz"
+        link: "https://corbingar.github.io/3-PasswordGenerator.github.io/",
+        github: "https://github.com/CorbinGar/3-PasswordGenerator/"
     }
 ]
 
