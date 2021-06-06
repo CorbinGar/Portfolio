eba5f02ff9a0a3bef460f3c9fe8cdd2410c0d4e7
Corbin Garner <Corbingar@gmail.com>
2021-06-06 00:12:55 -0700

    gotta love that detailing

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,9 +6,9 @@ function Navbar() {
     return (
 <nav className="navbar navbar-dark sticky-top navbar-expand-md">
 <div className="container-fluid">
-    <Link to="/" className="navbar-brand">
-        <h1 className="navbar-name">Mike Coletta</h1>
-    </Link>
+
+        <h1 className="navbar-name">Corbin Garner</h1>
+    
     <button className="navbar-toggler dropdownButton" type="button" data-toggle="collapse" data-target="#portfolio-navbar"
         aria-controls="portfolio-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon dropdownIcon"></span>
